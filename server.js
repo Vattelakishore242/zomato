79e59e58bfe9603b7f492b2a31f8337ac6c350de
Vattelakishore242 <147832974+Vattelakishore242@users.noreply.github.com>
2025-03-01 23:16:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const app=express()
+const PORT=3000
+
+app.listen(PORT,(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }
+    console.log("server is running on port :"+PORT)
+})
